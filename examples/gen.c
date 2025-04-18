1d4c91fa54f17ded3e235a3e29cf1871ff381a5e
RDCbum <81325149+RDCbum@users.noreply.github.com>
2025-04-18 03:25:19 +0200

    Add gen.c

---
+++ examples/gen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../src/saf_rng.h"
+
+int main(int argc, char **argv){
+    uint64_t n = (argc>1)?strtoull(argv[1],0,10):134217728ULL; /* 1 GiB */
+    saf_rng_seed(0xCAFEBABE);
+    for(uint64_t i=0;i<n;i++){
+        uint64_t z = saf_rng_u64();
+        fwrite(&z,8,1,stdout);
+    }
+    return 0;
+}
